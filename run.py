390bc5dfeb635c219951d28a33c62c40bbd16ff9
Sióg Davin Murphy <siogdavinmurphy@gmail.com>
2024-03-07 23:10:32 +0000

    add get_shopping_list function that pulls all the values from the shopping list and prints them to the terminal

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,15 +13,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('shopping-list')
 
-shopping = SHEET.worksheet('shopping')
+# FUNCTIONS
+
+def get_shopping_list():
+    """
+    Pulls all values from the shopping list
+    """
+    print("Loading your shopping list...\n")
+    shopping = SHEET.worksheet('shopping')
+    #gets all the values in the first column of the 'shopping' sheet
+    shopping_list_data = shopping.col_values(1)
+
+    for item in shopping_list_data:
+        print(item)
+    
+    print("")
+
+get_shopping_list()
 
 def add_new_items():
     """
     Get item names input from the user
     """
     print("Add items to your shopping list.")
-    print("Separate items with commas")
-    print("Example: Spinach, Ginger ale, Chocolate\n")
+    print("- Separate items with commas")
+    print("- Example: Spinach, Ginger ale, Chocolate\n")
 
     data_str = input("Add items to your list here: ")
     new_items = data_str.split(",")
