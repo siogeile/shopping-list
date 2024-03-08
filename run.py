87542aac6ab91c3a0490a8b1f8c43ac093e12f59
Sióg Davin Murphy <siogdavinmurphy@gmail.com>
2024-03-08 01:04:39 +0000

    Add main menu into a function and add convert to while loop

--- run.py
+++ run.py
@@ -51,42 +51,50 @@ def add_new_items():
     new_items = data_str.split(",")
     print(f"You have added {new_items} to your list")
 
-# MAIN MENU
+
 # Code copied with edits from Geek Tutorials on YouTube
+def main_menu():
+    """
+    Load a menu that will loop back once the functions in the
+    selections complete
+    """
+    while True:
+        print('''
 
-print("")
-print('''SHOPPING LIST
+        Select the number of the action that you would like to do:
 
-Select the number of the action that you would like to do:
+        1. View Shopping List
+        2. Add items to shopping list
+        3. View all items
+        4. Remove item from shopping list
+        5. Check if item is on shopping list
+        6. Clear Shopping List
+        7. Delete items
+        8. Exit''')
+        print("")
 
-1. View Shopping List
-2. Add items to shopping list
-3. View all items
-4. Remove item from shopping list
-5. Check if item is on shopping list
-6. Clear Shopping List
-7. Delete items
-8. Exit''')
-print("")
+        selection = input("What would you like to do?: ")
+        print("")
 
-selection = input("What would you like to do?: ")
-print("")
+        if selection == "1":
+            print_shopping_list()
+        elif selection == "2":
+            add_new_items()
+        elif selection == "3":
+            pass
+        elif selection == "4":
+            pass
+        elif selection == "5":
+            pass
+        elif selection == "6":
+            pass
+        elif selection == "7":
+            pass
+        elif selection == "8":
+            sys.exit()
+        else:
+            print("You did not make a valid selection")
 
-if selection == "1":
-    print_shopping_list()
-elif selection == "2":
-    add_new_items()
-elif selection == "3":
-    pass
-elif selection == "4":
-    pass
-elif selection == "5":
-    pass
-elif selection == "6":
-    pass
-elif selection == "7":
-    pass
-elif selection == "8":
-    sys.exit()
-else:
-    print("You did not make a valid selection")
+# MAIN MENU
+
+main_menu()
