79e8a8e6c070a4739b25f47bdfdd5e7ea2e5f3d7
Sióg Davin Murphy <siogdavinmurphy@gmail.com>
2024-03-09 11:55:07 +0000

    Fixed validation errors and debugged

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import sys
+import time
 
 
 SCOPE = [
@@ -17,6 +18,8 @@ SHEET = GSPREAD_CLIENT.open('shopping-list')
 # FUNCTIONS
 
 # Code copied with edits from Geek Tutorials on YouTube
+
+
 def main_menu():
     """
     Load a menu that will loop back once the functions in the
@@ -25,32 +28,39 @@ def main_menu():
     while True:
         clear_screen()
         print('''
-        Select the number of the action that you would like to do:
+        Enter a number to make a selection
 
         1. View Shopping List
         2. Add items to shopping list
         3. Remove item from shopping list
-        4. Clear previous entries
-        5. Exit''')
-        print("")
+        4. Exit''')
 
-        selection = input("What would you like to do?:\n ")
+        time.sleep(.3)
+        selection = input("\nWhat would you like to do?:\n ")
 
         if selection == "1":
+            clear_screen()
             print_shopping_list()
         elif selection == "2":
+            clear_screen()
             addition = add_new_items()
             update_worksheet(addition, 'shopping')
         elif selection == "3":
+            clear_screen()
             deduction = remove_items()
             update_removal(deduction, 'shopping')
         elif selection == "4":
             clear_screen()
-            main_menu()
-        elif selection == "5":
+            time.sleep(1)
+            sprint("See you soon")
+            time.sleep(2)
+            clear_screen()
             sys.exit()
         else:
-            print("You did not make a valid selection")
+            sprint("\nYou did not make a valid selection")
+
+        time.sleep(.05)
+
 
 def clear_screen():
     """
@@ -59,15 +69,28 @@ def clear_screen():
     print('\033[H\033[J')
     return
 
+
+def sprint(str):
+    """
+    Reduces the speed of text as it appears in the terminal
+    """
+    for c in str + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(3./90)
+
+
 def return_to_home(action_function):
-    print("\nDo you want to repeat this action or return home?")
-    return_home = input("Enter 'repeat' or 'home': ").lower()
-    if return_home == 'home':
-        return # exit loop
-    elif return_home == 'repeat':
-        action_function()
-    else:
-        print("You did not make a valid selection")
+    while True:
+        print("\nDo you want to go back to the main menu?")
+        return_home = input("y/n: ").lower()
+        if return_home == 'y':
+            return
+        elif return_home == 'n':
+            sprint("We can stay here for now")
+        else:
+            sprint("You did not make a valid selection")
+
 
 def shopping_list_items():
     """
@@ -78,17 +101,18 @@ def shopping_list_items():
 
     return shopping_list_data
 
+
 shopping_list = shopping_list_items()
+
 
 def print_shopping_list():
     """
     Prints the shopping list to the terminal
     """
-    clear_screen()
-    print("Loading your shopping list...\n")
+    sprint("Loading your shopping list...\n")
     print("--- SHOPPING LIST ---\n")
 
-    #retrieve shopping list updates
+    # retrieve shopping list updates
     shopping_list = shopping_list_items()
 
     for item in shopping_list:
@@ -96,14 +120,13 @@ def print_shopping_list():
 
     return_to_home(print_shopping_list)
 
+
 def add_new_items():
     """
     Get item names input from the user
     """
     while True:
-        clear_screen()
-        print("")
-        print("Add items to your shopping list.")
+        print("\nAdd items to your shopping list.")
         print("- Separate items with commas")
         print("- Example: Spinach, Ginger ale, Chocolate\n")
 
@@ -112,22 +135,18 @@ def add_new_items():
 
         if validate_data(new_items):
             print("Items validated")
+            sprint(f"You have added {new_items} to your list")
             break
 
-    print(f"You have added {new_items} to your list")
-    
     return new_items
 
-    return_to_home(add_new_items)
 
 def remove_items():
     """
     Get input for items to remove from the user
     """
     while True:
-        clear_screen()
-        print("")
-        print("Remove items to your shopping list.")
+        print("\nRemove items from your shopping list.")
         print("Be careful of the spelling")
         print("- Separate items with commas")
         print("- Example: Spinach, Ginger ale, Chocolate\n")
@@ -137,13 +156,11 @@ def remove_items():
 
         if validate_data(removed_items) and validate_removal(removed_items):
             print("Items located and validated")
+            sprint(f"You have removed {removed_items} from your list")
             break
 
-    print(f"You have removed {removed_items} from your list")
-    
     return removed_items
 
-    return_to_home(remove_items)
 
 def validate_data(words):
     """
@@ -153,19 +170,20 @@ def validate_data(words):
         for word in words:
             if len(str(word)) > 20:
                 raise ValueError(
-                    "Single items should be less than 20 characters"
+                    "One or more items exceeded 20 characters"
                 )
             elif not str(word).strip():
                 raise ValueError(
-                    "Entry invalid.\
-                        Please make sure all entries contain \
-                            at least one character each"
+                    "Did you enter an empty value?"
                 )
+                time.sleep(1)
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        time.sleep(1)
         return False
-    
+
     return True
+
 
 def validate_removal(words):
     """
@@ -176,20 +194,22 @@ def validate_removal(words):
     try:
         for word in words:
             if word not in shopping_list:
-                raise ValueError(f"{word} not found on the shopping list"
-            )
+                raise ValueError(f"{word} not found on the shopping list")
+            time.sleep(.5)
     except ValueError as e:
         print(f"Invalid removal: {e}, please try again.\n")
+        time.sleep(.5)
         return False
 
     return True
+
 
 def update_worksheet(data, worksheet):
     """
     Receive a list of values to insert into a worksheet
     Update the relevant worksheet with the data provided
     """
-    print(f"Updating {worksheet} worksheet...\n")
+    sprint(f"Updating {worksheet} list...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
 
     # get existing values in the first row
@@ -201,22 +221,25 @@ def update_worksheet(data, worksheet):
     # amend the first row with the updated values
     worksheet_to_update.update([updated_values], '1:1')
 
-    print(f"{worksheet} worksheet updated successfully\n")
+    print(f"Your {worksheet} list updated successfully\n")
+    time.sleep(1)
+
 
 def update_removal(data, worksheet):
     """
     Receive a list of values to remove from a worksheet
     Update the relevant worksheet by removing the specified items
     """
-    print(f"Updating {worksheet} list...\n")
+    sprint(f"Updating {worksheet} list...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
 
     # get existing values in the first row
     existing_values = worksheet_to_update.row_values(1)
 
     # Create a new list excluding the items to be removed
-    updated_values = [item for item in\
-        worksheet_to_update.row_values(1) if item not in data]
+    updated_values = [
+        item for item in worksheet_to_update.row_values(1) if item not in data
+        ]
 
     # get number of values in the list before deductions
     num = len(existing_values)
@@ -228,7 +251,14 @@ def update_removal(data, worksheet):
     worksheet_to_update.update([updated_values], '1:1')
 
     print(f"{worksheet} list updated successfully\n")
+    time.sleep(1)
 
 # MAIN MENU
 
+
+clear_screen()
+print("\nWelcome!")
+time.sleep(2)
+sprint("\nYour shopping list application is loading...")
+time.sleep(.5)
 main_menu()
